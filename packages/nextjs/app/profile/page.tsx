dcaa3896016da00563ad1e46b31d266a5367806e
Dharmik79 <ddholariya199@gmail.com>
2024-04-18 00:31:26 -0600

    added the event details on th eproffile page

--- packages/nextjs/app/profile/page.tsx
+++ packages/nextjs/app/profile/page.tsx
@@ -10,29 +10,35 @@ const Profile = () => {
   const router = useRouter();
   const { address: connectedAddress } = useAccount();
 
-  // const { data: eventDetails } = useScaffoldContractRead({
-  //   contractName: "EASOnboarding",
-  //   functionName: "events",
-  //   args: [1n],
-  // });
-
-  const { data: attestationData } = useScaffoldContractRead({
+  const { data: eventDetails } = useScaffoldContractRead({
     contractName: "EASOnboarding",
-    functionName: "getEventsCompleted",
+    functionName: "getAllStudentEventsWithAttestations",
     args: [connectedAddress],
   });
+
+  // const { data: attestationData } = useScaffoldContractRead({
+  //   contractName: "EASOnboarding",
+  //   functionName: "getEventsCompleted",
+  //   args: [connectedAddress],
+  // });
 
   return (
     <div>
       <div className="my-12 grid items-center justify-center xl:m-10 md:grid-cols-3  ">
-        {attestationData?.[2]?.map((doc, index) => {
+        {eventDetails?.map((doc, index) => {
           return (
-            <div className="overflow-hidden rounded-md m-2 bg-gray-200/60" key={index}>
+            <div className="overflow-hidden rounded-md m-2 bg-gray-200/60 cursor-pointer" key={index}>
               <div className="px-6 py-6">
                 <div className="flex justify-center items-center">
                   <div className="flex flex-row items-center justify-between">
                     <div className="mr-4">
                       <Image src="/badge1.png" alt="Profile" width={300} height={300} className="rounded-full" />
+                    </div>
+
+                    <div>
+                      <div>Event Name:{doc?.eventName}</div>
+                      <div>Event Description:{doc?.eventDescription}</div>
+                      <div>Mentor Name:{doc?.mentorName}</div>
                     </div>
                   </div>
                 </div>
@@ -41,7 +47,7 @@ const Profile = () => {
                     <a
                       className="text-xl"
                       style={{ marginTop: "-1rem" }}
-                      href={`${process.env.NEXT_PUBLIC_ATTESTATION_VIEW_URL}/${doc}`}
+                      href={`${process.env.NEXT_PUBLIC_ATTESTATION_VIEW_URL}/${doc?.attestation}`}
                     >
                       See on EAS explorer
                     </a>
