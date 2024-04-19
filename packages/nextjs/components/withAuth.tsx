165d26780fb6d1cd3b79d69f2aa0f5e41bed90fa
Dharmik79 <ddholariya199@gmail.com>
2024-04-19 09:03:38 -0600

    added the owner address

--- packages/nextjs/components/withAuth.tsx
+++ packages/nextjs/components/withAuth.tsx
@@ -17,6 +17,9 @@ export const withAuth = (WrappedComponent: React.FC<any>) => {
         "0xdA7773E91a396d592AD33146164dA6d7d2Fda9B6",
         "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
         "0xbd53471abf1C3827b8EeEF4898fcfe76821a3C2B",
+        "0x04cc6b487566B1C821bEa04d7ac0d23CEDe05cC9",
+        "0xeCB4C1245665e8A1F43826355aaB0Dd6bF336e05",
+        "0xe2A45CA9Ec5780FC389FBD8991980397b8B470AF",
       ].includes(connectedAddress) &&
       (pathname == "/create-quiz" || pathname == "/create-event")
     ) {
