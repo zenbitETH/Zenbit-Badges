d2b4db40262cce0fc5a7d73b6daa8368ee6026e9
Dharmik79 <ddholariya199@gmail.com>
2024-05-02 13:18:32 -0600

    added the image URI in the profile page

--- packages/nextjs/app/profile/page.tsx
+++ packages/nextjs/app/profile/page.tsx
@@ -26,17 +26,14 @@ const Profile = () => {
   return (
     <div>
       <div className="my-12 grid items-center justify-center xl:m-10 md:grid-cols-2 2xl:grid-cols-3  ">
-        {eventDetails?.map((doc, index) => {
-          const src = Number(doc?.eventId) == 1 ? "/badge1.png" : "/badge2.png";
-          // const src = "https://ipfs.io/ipfs/"+doc.imgUri;
-
+        {eventDetails?.[0]?.map((doc, index) => {
           return (
             <div className="overflow-hidden rounded-md m-2 bg-gray-200/60 text-center p-6 relative" key={index}>
               <div className="absolute bottom-0 left-0 bg-zen rounded-tr-md rounded-bl-md px-4 py-1 font-mus text-sm xl:text-base">
-                Evento
+                Evento {doc?.eventId.toString()}
               </div>
               <div className="absolute bottom-0 right-0 bg-bit rounded-br-md rounded-tl-md px-4 py-1 text-white font-mus text-sm xl:text-base">
-                Nv: 0
+                Nv: {doc?.level.toString()}
               </div>
               <div className="absolute bottom-0 right-0 left-0 text-center bg-white hover:bg-gray-500 rounded-t-md w-fit mx-auto px-4 py-1 hover:text-white text-sm xl:text-base">
                 <a
@@ -49,8 +46,14 @@ const Profile = () => {
                 </a>
               </div>
               <div className="xl:text-2xl md:text-xl font-bold font-mus ">{doc?.eventName}</div>
-              <Image src={src} alt="Profile" width={300} height={300} className="rounded-full mx-auto my-3" />
-              <div className="text-xl italic pb-6">Mentor: habacuc.eth</div>
+              <Image
+                src={"https://ipfs.io/ipfs/" + eventDetails?.[1]?.[index]}
+                alt="Profile"
+                width={300}
+                height={300}
+                className="rounded-full mx-auto my-3"
+              />
+              <div className="text-xl italic pb-6">Mentor: {doc.mentorName}</div>
             </div>
           );
         })}
