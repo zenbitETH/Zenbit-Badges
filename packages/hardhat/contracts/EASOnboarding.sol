b6d2320536a89e4602543c837928e16fad3e4b01
shyam-patel-kira <shyampkira@gmail.com>
2024-05-02 16:23:05 -0400

    remove lib

--- packages/hardhat/contracts/EASOnboarding.sol
+++ packages/hardhat/contracts/EASOnboarding.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "./lib/Structs.sol";
 import "./storage/EASOnboardingStorage.sol";
 
